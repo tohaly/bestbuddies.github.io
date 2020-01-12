75b97fbd7fbf5b8b30bda1f87aebb6492df5c741
Anton Voitenko <voytenko.ad@yandex.ru>
2020-01-13 01:38:01 +0800

    Fix layout bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 import '../pages/index.css';
 import './slider';
-import {Header} from './header';
+import { Header } from './header';
 import Popup from './popup';
 
 const popupContainer = document.querySelector('.popup');
@@ -11,18 +11,18 @@ const popup = new Popup(popupContainer);
 headerFunctional.listeners();
 
 document.querySelectorAll('.help-button').forEach(button => {
-  button.addEventListener('click', popup.open);
+  button.addEventListener('click', popup.open)
 });
 
 document.querySelector('.up-button').addEventListener('click', event => {
   event.preventDefault();
   const timeOut = setInterval(() => {
-          window.scrollBy(0,-100);          
-        },10);
+    window.scrollBy(0, -100);
+  }, 10);
   document.addEventListener('scroll', () => {
-    if(pageYOffset === 0){          
+    if (pageYOffset === 0) {
       clearInterval(timeOut);
     }
-  })  
-})
+  });
+});
 
