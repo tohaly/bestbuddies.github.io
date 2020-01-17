442ca6e5c556f4836c26d98c76a5f03ac0dcc0a4
Andrew Lapshov <titan1um@yandex.ru>
2020-01-17 21:36:02 +0300

    17.01: progressbar API added

--- src/js/api.js
+++ src/js/api.js
@@ -33,26 +33,3 @@ export default class Api {
     });
   }
 }
-
-// .then(res => {
-//       const currentSum = res.donations.reduce((acc, item) => {
-//         return acc + item.amount;
-//       }, 0);
-//       const progressBar = document.querySelector(
-//         '.form__donation-bar-progress',
-//       );
-//       const currentSumDOM = document.querySelector(
-//         '.form__donation-sum-current',
-//       );
-//       const maxSumDOM = document.querySelector('.form__donation-sum-max');
-//       const maxSum = Number(maxSumDOM.textContent);
-//       console.log(maxSum);
-//       if (currentSum <= maxSum) {
-//         progressBar.style.width = `${(currentSum * 100) / maxSum}%`;
-//         currentSumDOM.textContent = currentSum.toLocaleString();
-//       } else {
-//         progressBar.style.width = `100%`;
-//         currentSumDOM.textContent = maxSum.toLocaleString();
-//       }
-//       maxSumDOM.textContent = maxSum.toLocaleString();
-//     })
