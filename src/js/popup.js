996c99421ac2490ff136455fc408beff8c77504a
Anton Voitenko <voytenko.ad@yandex.ru>
2020-01-23 20:29:57 +0800

    Deleted comments

--- src/js/popup.js
+++ src/js/popup.js
@@ -25,43 +25,4 @@ export default class Popup {
     this.toggleEventListeners(false);
     this.container.classList.remove('popup_is-opened');
   }
-
-  // onSuccess(e) {
-  //   if (e.target.closest('.popup')) {
-  //     window.formPopup.close();
-  //   }
-  //   window.successPopup.open();
-  // }
-
-  // open() {
-  //   this.render();
-  //   this.popupForm.insertToPopup();
-  //   this.container.appendChild(this.popupForm.form);
-  // }
-
-  // close(e) {
-  //   // if (
-  //   //   !e.target.nextElementSibling.classList.contains('popup__form_success')
-  //   // ) {
-  //   //   this.popupForm.removeListeners();
-  //   // }
-  //   this.removeListeners(e);
-  //   this.closeRender();
-  //   this.container.classList.remove('popup_is-opened');
-  // }
-
-  // renderSuccessPopup() {
-  //   if (this.container.classList.contains('popup_is-opened')) {
-  //     this.container.querySelector('.popup__form').remove();
-  //   } else {
-  //     this.container.classList.add('popup_is-opened');
-  //   }
-
-  //   const successPopup = document
-  //     .querySelector('.popup-template__success')
-  //     .content.cloneNode(true)
-  //     .querySelector('.popup__form');
-  //   this.container.querySelector('.popup__content').appendChild(successPopup);
-  //   window.Sharer.init();
-  // }
 }
