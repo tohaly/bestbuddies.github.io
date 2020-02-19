39721e283349b824cb25b35cc845379ab290aff7
Andrew Lapshov <titan1um@yandex.ru>
2020-02-19 21:54:40 +0300

    3-5 fixes added

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@ export default {
   SERVER_URL:
     'https://v2-api.sheety.co/2913ff0e1453a0ecead83d09ae6c935b/bestFriends/donations',
   TOKEN: 'Bearer praktikum2020',
-  PUBLIC_ID: 'test_api_00000000000000000000001',
+  PUBLIC_ID: 'pk_9f8beef30a960e9cea89d50605174',
   MAX_SUM: 764536,
 };
