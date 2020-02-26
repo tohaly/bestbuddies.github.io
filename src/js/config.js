c467456c42a7a84881c49ae5738de403180296ef
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-26 21:02:42 +0800

    Reconfigured payment

--- src/js/config.js
+++ src/js/config.js
@@ -2,6 +2,6 @@ export default {
   SERVER_URL:
     'https://v2-api.sheety.co/2913ff0e1453a0ecead83d09ae6c935b/bestFriends/donations',
   TOKEN: 'Bearer praktikum2020',
-  PUBLIC_ID: 'pk_9f8beef30a960e9cea89d50605174',
+  PUBLIC_ID: 'pk_04bff5ed469f8968d3c9d08e59af6',
   MAX_SUM: 764536,
 };
