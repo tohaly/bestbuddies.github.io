67bfbeffda1991abad1210f3623394a28aab65f3
Anton Voitenko <voytenko.ad@yandex.ru>
2020-02-28 02:22:57 +0800

    Debuging video

--- src/js/video.js
+++ src/js/video.js
@@ -3,6 +3,7 @@ export default class YoutubeApiRemote {
     this.playButton = document.querySelector('.lead__button');
     this.fixBugWithLoad();
     this.counter = 0;
+    this.isRepeat = false;
     this.youtubeIfreameApi();
     this.switchingButton = this.switchingButton.bind(this);
     this.listeners = this.listeners.bind(this);
@@ -20,6 +21,7 @@ export default class YoutubeApiRemote {
           mute: 1,
           loop: 1,
           controls: 0,
+          disablekb: 1,
           playlist: '5tfGHKGXBiw',
           origin: window.location.href,
         },
@@ -37,6 +39,7 @@ export default class YoutubeApiRemote {
 
   // eslint-disable-next-line class-methods-use-this
   getUnmutedVideo() {
+    window.player.isButtonRun = true;
     const iframe = document.querySelector('.lead__iframe');
     const palceholder = document.querySelector('.lead__video');
     iframe.classList.add('lead__iframe_visible');
@@ -44,26 +47,38 @@ export default class YoutubeApiRemote {
     window.player.playVideoAt();
     window.player.unMute();
     window.player.playVideo();
-    // this.classList.add('lead__button_hidden');
   }
 
   switchingButton(event) {
     // eslint-disable-next-line default-case
     switch (event.data) {
+      case 1: {
+        if ((this.counter > 1 && this.isRepeat) || window.player.isButtonRun) {
+          this.playButton.classList.add('lead__button_hidden');
+        }
+        this.counter += 1;
+        break;
+      }
       case 2: {
+        this.playButton.classList.remove('lead__button_hidden');
+        return;
+      }
+      case 0: {
+        if (this.counter >= 1) {
+          this.counter += 1;
+          this.playButton.classList.add('lead__button_hidden');
+        }
+        break;
+      }
+      case -1: {
+        this.counter = 0;
         this.playButton.classList.remove('lead__button_hidden');
         break;
       }
       case 3: {
-        this.playButton.classList.add('lead__button_hidden');
-        break;
-      }
-      case 1: {
         this.counter += 1;
-        if (this.counter > 1) {
-          this.playButton.classList.add('lead__button_hidden');
-          break;
-        }
+        this.isRepeat = true;
+        break;
       }
     }
   }
